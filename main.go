06e842195b21daf7b44650b1367f590609cc7314
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2025-07-07 00:48:35 +0000

    introducing format issue

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", serveHTML)()
 	r.HandleFunc("/time", timeHandler)
-	log.Println("Starting server on :8080")
+			log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
