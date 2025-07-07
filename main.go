2dbd5d648fd61e1f6a8dba2b4366e3047767baa3
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2025-07-07 01:09:20 +0000

    removing format issue

--- main.go
+++ main.go
@@ -56,9 +56,9 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Hello, World!")
 	r := mux.NewRouter()
-	r.HandleFunc("/", serveHTML)()
+	r.HandleFunc("/", serveHTML)
 	r.HandleFunc("/time", timeHandler)
-			log.Println("Starting server on :8080")
+	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
