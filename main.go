d9e817159b484668f08046c2dcc7f4753faae9ca
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2025-07-07 09:31:16 +0000

    fixed lint issues

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 	fmt.Println("Hello, World!")
 	r := mux.NewRouter()
 	r.HandleFunc("/", serveHTML)
-			r.HandleFunc("/time", timeHandler)()
+	r.HandleFunc("/time", timeHandler)
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
